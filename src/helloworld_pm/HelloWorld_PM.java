137ecf4d5e1ca151c72907dc8f200c887d0134a9
Pranjal <mohanep@sheridancollege.ca>
2023-05-18 14:44:22 -0400

    My first commit

--- src/helloworld_pm/HelloWorld_PM.java
+++ src/helloworld_pm/HelloWorld_PM.java
@@ -14,7 +14,7 @@ public class HelloWorld_PM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Wassup");
             }
     
 }
